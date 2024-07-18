ca422cab9753e70d013a2b727389300841d1d62e
ederson <ilaferezizli2@gmail.com>
2024-07-18 22:20:29 +0400

    yeni

---
+++ app/src/main/java/com/activity/butabu/activities/ProhibitedWords.kt
@@ -0,0 +1,5 @@
+package com.activity.butabu.activities
+
+data class ProhibitedWords(
+    val words: List<String>
+)
---
+++ app/src/main/java/com/activity/butabu/objects/Rounds.kt
@@ -0,0 +1,6 @@
+package com.activity.butabu.objects
+
+object Rounds {
+    var roundTotal = 4
+    var roundCurrent = 1
+}
